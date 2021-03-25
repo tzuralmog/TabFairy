6444bbe5eb80758c1d2fc04f2c25c75b6cbe8654
HELIKAS\Nexxa <nexxa9@outlook.com>
2021-03-25 17:46:33 -0400

    CSS Work

--- webextensions/sidebar/sidebar.js
+++ webextensions/sidebar/sidebar.js
@@ -1,3 +1,11 @@
 document.getElementById('new-group').addEventListener('click', function(){
-    document.getElementById('empty').remove();
+    //if instruction text is there, remove it
+    if(document.getElementById('empty'))
+        document.getElementById('empty').remove();
+
+    //add a new group
+    var ul = document.getElementById("groups");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("New Group"));
+    ul.appendChild(li);
 });
