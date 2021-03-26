0d6d00b225f6007e06e8a152409590ecb95df395
tzur <Tzur.almog@gmail.com>
2021-03-26 13:55:45 -0400

    added tablist

--- webextensions/sidebar/sidebar.js
+++ webextensions/sidebar/sidebar.js
@@ -1,3 +1,47 @@
+// runs the tabs list on startup
+document.addEventListener("DOMContentLoaded", listTabs);
+browser.tabs.onUpdated.addListener(listTabs);
+browser.tabs.onRemoved.addListener(listTabsWithRemove);
+browser.tabs.onCreated.addListener(listTabs);
+browser.tabs.onMoved.addListener(listTabs);
+
+function getCurrentWindowTabs() {
+  return browser.tabs.query({currentWindow: true});
+}
+
+function listTabs( ) {
+  getCurrentWindowTabs().then((tabs) => {
+     let tabsList = document.getElementById('tab-list');
+     let currentTabs = document.createDocumentFragment();
+     tabsList.textContent = '';
+     for (let tab of tabs) {
+      let tabLink = document.createElement('a');
+      tabLink.textContent = tab.title || tab.id;
+      currentTabs.appendChild(tabLink);
+      let spacing = document.createElement('br');
+      currentTabs.appendChild(spacing);
+     }
+     tabsList.appendChild(currentTabs);
+  });
+}
+function listTabsWithRemove(tabId, removeInfo) {
+  getCurrentWindowTabs().then((tabs) => {
+     let tabsList = document.getElementById('tab-list');
+     let currentTabs = document.createDocumentFragment();
+     tabsList.textContent = '';
+     for (let tab of tabs) {
+      if (tab.id != tabId) {
+      let tabLink = document.createElement('a');
+      tabLink.textContent = tab.title || tab.id;
+      currentTabs.appendChild(tabLink);
+      let spacing = document.createElement('br');
+      currentTabs.appendChild(spacing);
+      }
+     }
+     tabsList.appendChild(currentTabs);
+  });
+}
+
 document.getElementById('new-group').addEventListener('click', function(){
     //if instruction text is there, remove it
     if(document.getElementById('empty'))
@@ -10,7 +54,8 @@ document.getElementById('new-group').addEventListener('click', function(){
     ul.appendChild(li);
 });
 
-document.getElementById('new-tab').addEventListener('click', function(){
+document.getElementById('list-tabs').addEventListener('click', function(){
   // testing
-  browser.tabs.create();
+  // browser.tabs.create();
+  listTabs();
 });
