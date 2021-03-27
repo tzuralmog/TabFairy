9e6e0b6e2b23bdfde35db6b70a51ab0497f816c3
HELIKAS\Nexxa <nexxa9@outlook.com>
2021-03-27 11:38:54 -0400

    Remove Group Button

--- webextensions/sidebar/sidebar.js
+++ webextensions/sidebar/sidebar.js
@@ -10,8 +10,12 @@ function createNewGroup(){
     var ul = document.getElementById("groups");
     var li = document.createElement("li");
     var btn = document.createElement("button");
-    btn.setAttribute("id", "rename");
-    btn.innerHTML = "Rename";
+    btn.setAttribute("class", "remove");
+    btn.innerHTML = "Remove";
+    btn.onclick = function(){
+        btn.parentElement.remove()
+        return;
+    };
     li.appendChild(document.createTextNode("New Group"));
     li.appendChild(btn);
     ul.appendChild(li);
