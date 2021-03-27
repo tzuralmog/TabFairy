0d867530924fc955cf0fc44e2cf4b7bb16ce12c4
HELIKAS\Nexxa <nexxa9@outlook.com>
2021-03-27 11:16:16 -0400

    Rename Button

--- webextensions/sidebar/sidebar.js
+++ webextensions/sidebar/sidebar.js
@@ -3,9 +3,16 @@ document.getElementById('new-group').addEventListener('click', function(){
     if(document.getElementById('empty'))
         document.getElementById('empty').remove();
 
-    //add a new group
+    createNewGroup();
+});
+
+function createNewGroup(){
     var ul = document.getElementById("groups");
     var li = document.createElement("li");
+    var btn = document.createElement("button");
+    btn.setAttribute("id", "rename");
+    btn.innerHTML = "Rename";
     li.appendChild(document.createTextNode("New Group"));
+    li.appendChild(btn);
     ul.appendChild(li);
-});
+}
