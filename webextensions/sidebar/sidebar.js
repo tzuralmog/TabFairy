527f94c22775a5c39e1671e4a8221255ce6a6b0c
tzur <Tzur.almog@gmail.com>
2021-03-28 10:16:30 -0400

    trying to get listtabs not to fire after remove

--- webextensions/sidebar/sidebar.js
+++ webextensions/sidebar/sidebar.js
@@ -1,47 +1,4 @@
-// runs the tabs list on startup
-document.addEventListener("DOMContentLoaded", listTabs);
-browser.tabs.onUpdated.addListener(listTabs);
-browser.tabs.onRemoved.addListener(listTabsWithRemove);
-browser.tabs.onCreated.addListener(listTabs);
-browser.tabs.onMoved.addListener(listTabs);
 
-function getCurrentWindowTabs() {
-  return browser.tabs.query({currentWindow: true});
-}
-
-function listTabs( ) {
-  getCurrentWindowTabs().then((tabs) => {
-     let tabsList = document.getElementById('tab-list');
-     let currentTabs = document.createDocumentFragment();
-     tabsList.textContent = '';
-     for (let tab of tabs) {
-      let tabLink = document.createElement('a');
-      tabLink.textContent = tab.title || tab.id;
-      currentTabs.appendChild(tabLink);
-      let spacing = document.createElement('br');
-      currentTabs.appendChild(spacing);
-     }
-     tabsList.appendChild(currentTabs);
-  });
-}
-function listTabsWithRemove(tabId, removeInfo) {
-  getCurrentWindowTabs().then((tabs) => {
-     document.getElementById('tab-list').innerText = "Changed";
-     let tabsList = document.getElementById('tab-list');
-     let currentTabs = document.createDocumentFragment();
-     tabsList.textContent = '';
-     for (let tab of tabs) {
-      if (tab.id != tabId) {
-      let tabLink = document.createElement('a');
-      tabLink.textContent = tab.title || tab.id;
-      currentTabs.appendChild(tabLink);
-      let spacing = document.createElement('br');
-      currentTabs.appendChild(spacing);
-      }
-     }
-     tabsList.appendChild(currentTabs);
-  });
-}
 
 document.getElementById('new-group').addEventListener('click', function(){
     //if instruction text is there, remove it
@@ -75,7 +32,6 @@ function createNewGroup(){
     ul.appendChild(li);
 };
 
-document.getElementById('list-tabs').addEventListener('click', function(){
-  // testing
-  listTabs();
-});
+
+
+
